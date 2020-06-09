ca3fd77eaf29300a530909bf923c4a6797b7e48c
Xiaojun Wu <xiaojunw@usc.edu>
2020-06-08 17:26:49 -0700

    Add clipping to the standard deviation of prior when sampling from a cell list

--- stan_calcium_model.py
+++ stan_calcium_model.py
@@ -17,6 +17,11 @@ def main():
     filter_type = args.filter_type
     moving_average_window = args.moving_average_window
     t0 = args.t0
+    downsample_offset = args.downsample_offset
+    prior_dir = args.prior_dir
+    prior_chains = args.prior_chains
+    prior_spec_path = args.prior_spec
+    prior_std_scale = args.prior_std_scale
     num_chains = args.num_chains
     num_iters = args.num_iters
     warmup = args.warmup
@@ -24,32 +29,16 @@ def main():
     adapt_delta = args.adapt_delta
     max_treedepth = args.max_treedepth
     result_dir = args.result_dir
-    prior_dir = args.prior_dir
-    prior_chains = args.prior_chains
-    prior_spec_path = args.prior_spec
-    prior_std_scale = args.prior_std_scale
     analysis_tasks = args.analysis_tasks
     use_summary = args.use_summary
 
     # prepare data for Stan model
     print("Initializing data for cell {}...".format(cell_id))
     # get trajectory and time
-    # print("Loading trajectories...")
-    # y = np.loadtxt("canorm_tracjectories.csv", delimiter=",")
-    # y = y[cell_id, :]
-    # t_end = y.size - 1
-    # # apply preprocessing to the trajectory if specified
-    # if filter_type == "moving_average":
-    #     y = moving_average(y, window=moving_average_window)
-    #     y = np.squeeze(y)
-    # ts = np.linspace(t0 + 1, t_end, t_end - t0)
-    # # downsample trajectories
-    # t_downsample = 300
-    # y = np.concatenate((y[0:t_downsample], y[t_downsample::10]))
-    # ts = np.concatenate((ts[0:t_downsample-t0], ts[t_downsample-t0::10]))
     y, y0_ca, ts = load_trajectories(
         t0, filter_type=filter_type,
-        moving_average_window=moving_average_window, downsample_offset=300)
+        moving_average_window=moving_average_window,
+        downsample_offset=downsample_offset)
     y = y[cell_id, :]
     y0_ca = y0_ca[cell_id]
     param_names = ["sigma", "KonATP", "L", "Katp", "KoffPLC", "Vplc", "Kip3",
@@ -60,8 +49,8 @@ def main():
 
     # get prior distribution
     if prior_dir:
-        prior_mean, prior_std = get_prior_from_sample_files(prior_dir,
-                                                            prior_chains)
+        prior_mean, prior_std = get_prior_from_sample_files(
+            prior_dir, prior_chains)
     elif prior_spec_path:
         print(f"Getting prior distribution from {prior_spec_path}...")
         prior_spec = pd.read_csv(prior_spec_path, delimiter="\t", index_col=0)
@@ -131,48 +120,34 @@ def get_args():
     """parse command line arguments"""
     arg_parser = argparse.ArgumentParser(
         description="Infer parameters of calcium mode using stan.")
-    arg_parser.add_argument("--stan_model", dest="stan_model", metavar="MODEL",
-                            type=str, required=True)
-    arg_parser.add_argument("--ode_variant", dest="ode_variant", type=str,
-                            default="vanilla",
+    arg_parser.add_argument("--stan_model", metavar="MODEL", type=str,
+                            required=True)
+    arg_parser.add_argument("--ode_variant", type=str, default="vanilla",
                             choices=["vanilla", "equiv", "const"])
-    arg_parser.add_argument("--stan_backend", dest="stan_backend",
-                            metavar="BACKEND", type=str, default="pystan",
-                            choices=["pystan", "cmdstanpy"])
-    arg_parser.add_argument("--cell_id", dest="cell_id", metavar="N", type=int,
-                            default=0)
-    arg_parser.add_argument("--filter_type", dest="filter_type",
-                            choices=["none", "moving_average"], default="none")
-    arg_parser.add_argument("--moving_average_window",
-                            dest="moving_average_window", type=int, default=20)
-    arg_parser.add_argument("--t0", dest="t0", metavar="T", type=int,
-                            default=200)
-    arg_parser.add_argument("--num_chains", dest="num_chains", type=int,
-                            default=4)
-    arg_parser.add_argument("--num_iters", dest="num_iters", type=int,
-                            default=2000)
-    arg_parser.add_argument("--warmup", dest="warmup", type=int, default=1000)
-    arg_parser.add_argument("--thin", dest="thin", type=int, default=1)
-    arg_parser.add_argument("--adapt_delta", dest="adapt_delta", type=float,
-                            default=0.8)
-    arg_parser.add_argument("--max_treedepth", dest="max_treedepth", type=int,
-                            default=10)
-    arg_parser.add_argument("--result_dir", dest="result_dir", metavar="DIR",
-                            type=str, default=".")
-    arg_parser.add_argument("--prior_dir", dest="prior_dir", type=str,
-                            default=None)
-    arg_parser.add_argument("--prior_chains", dest="prior_chains", type=int,
-                            nargs="+", default=[0, 1, 2, 3])
-    arg_parser.add_argument("--prior_spec", dest="prior_spec", type=str,
-                            default=None)
-    arg_parser.add_argument("--prior_std_scale", dest="prior_std_scale",
-                            type=float, default=1.0)
-    arg_parser.add_argument("--analysis_tasks", dest="analysis_tasks",
-                            nargs="+",
+    arg_parser.add_argument("--stan_backend",  metavar="BACKEND", type=str,
+                            default="pystan", choices=["pystan", "cmdstanpy"])
+    arg_parser.add_argument("--cell_id", type=int, default=0)
+    arg_parser.add_argument("--filter_type", default=None,
+                            choices=["moving_average"])
+    arg_parser.add_argument("--moving_average_window", type=int, default=20)
+    arg_parser.add_argument("--t0", type=int, default=200)
+    arg_parser.add_argument("--downsample_offset", type=int, default=300)
+    arg_parser.add_argument("--prior_dir", type=str, default=None)
+    arg_parser.add_argument("--prior_chains", type=int, nargs="+",
+                            default=[0, 1, 2, 3])
+    arg_parser.add_argument("--prior_spec", type=str, default=None)
+    arg_parser.add_argument("--prior_std_scale", type=float, default=1.0)
+    arg_parser.add_argument("--num_chains", type=int, default=4)
+    arg_parser.add_argument("--num_iters", type=int, default=2000)
+    arg_parser.add_argument("--warmup", type=int, default=1000)
+    arg_parser.add_argument("--thin", type=int, default=1)
+    arg_parser.add_argument("--adapt_delta",  type=float, default=0.8)
+    arg_parser.add_argument("--max_treedepth", type=int, default=10)
+    arg_parser.add_argument("--result_dir", type=str, required=True)
+    arg_parser.add_argument("--analysis_tasks", nargs="+", default=None,
                             choices=["all", "simulate_chains",
-                                     "plot_parameters", "get_r_squared"],
-                            default=None)
-    arg_parser.add_argument("--use_summary", dest="use_summary", default=False,
+                                     "plot_parameters", "get_r_squared"])
+    arg_parser.add_argument("--use_summary", default=False,
                             action="store_true")
 
     return arg_parser.parse_args()
--- stan_calcium_model_multiple.py
+++ stan_calcium_model_multiple.py
@@ -4,8 +4,9 @@ import os
 import argparse
 import numpy as np
 import pandas as pd
-from stan_helpers import StanSession, StanSessionAnalyzer, moving_average, \
-    get_prior_from_sample_files, calcium_ode_vanilla, calcium_ode_equiv
+import stan_helpers
+from stan_helpers import StanSession, StanSessionAnalyzer, load_trajectories, \
+    get_prior_from_sample_files
 
 def main():
     # get command-line arguments
@@ -13,12 +14,14 @@ def main():
     stan_model = args.stan_model
     ode_variant = args.ode_variant
     cell_list_path = args.cell_list
-    prior_id = args.prior_cell
-    prior_chains = args.prior_chains
     num_cells = args.num_cells
     filter_type = args.filter_type
     moving_average_window = args.moving_average_window
     t0 = args.t0
+    downsample_offset = args.downsample_offset
+    prior_id = args.prior_cell
+    prior_chains = args.prior_chains
+    prior_std_scale = args.prior_std_scale
     num_chains = args.num_chains
     num_iters = args.num_iters
     warmup = args.warmup
@@ -27,7 +30,6 @@ def main():
     max_treedepth = args.max_treedepth
     rhat_upper_bound = args.rhat_upper_bound
     result_dir = args.result_dir
-    prior_std_scale = args.prior_std_scale
 
     # set up for sampling
     cell_list = pd.read_csv(cell_list_path, delimiter="\t", index_col=False)
@@ -44,29 +46,19 @@ def main():
               + "to the latter")
     first_cell_order = prior_cell_order + 1
     last_cell_order = prior_cell_order + num_cells
-    prior_dir = os.path.join(result_dir, "cell-{:04d}".format(prior_id))
+    prior_dir = os.path.join(result_dir, f"cell-{prior_id:04d}")
 
     # get trajectory and time
-    print("Loading trajectories...")
-    y = np.loadtxt("canorm_tracjectories.csv", delimiter=",")
-    t_end = y.shape[1] - 1
-    # apply preprocessing to the trajectory if specified
-    if filter_type == "moving_average":
-        y = moving_average(y, window=moving_average_window)
-    ts = np.linspace(t0 + 1, t_end, t_end - t0)
-    # downsample trajectories
-    t_downsample = 300
-    y = np.concatenate((y[:, 0:t_downsample], y[:, t_downsample::10]), axis=1)
-    ts = np.concatenate((ts[0:t_downsample-t0], ts[t_downsample-t0::10]))
+    y, y0_ca, ts = load_trajectories(
+        t0, filter_type=filter_type,
+        moving_average_window=moving_average_window,
+        downsample_offset=downsample_offset, verbose=True)
     T = ts.size
     param_names = ["sigma", "KonATP", "L", "Katp", "KoffPLC", "Vplc", "Kip3",
                    "KoffIP3", "a", "dinh", "Ke", "Be", "d1", "d5", "epr",
                    "eta1", "eta2", "eta3", "c0", "k3"]
     var_names = ["PLC", "IP3", "h", "Ca"]
-    if ode_variant == "equiv":
-        calcium_ode = calcium_ode_equiv
-    else:
-        calcium_ode = calcium_ode_vanilla
+    calcium_ode = getattr(stan_helpers, "calcium_ode_" + ode_variant)
     control = {"adapt_delta": adapt_delta, "max_treedepth": max_treedepth}
 
     max_num_tries = 3  # maximum number of tries of stan sampling
@@ -77,36 +69,37 @@ def main():
     for cell_order in range(first_cell_order, last_cell_order + 1):
         # set up for current cell
         cell_id = cell_list.loc[cell_order, "Cell"]
-        cell_dir = os.path.join(result_dir, "cell-{:04d}".format(cell_id))
+        cell_dir = os.path.join(result_dir, f"cell-{cell_id:04d}")
         if not os.path.exists(cell_dir):
             os.mkdir(cell_dir)
         num_tries = 0
-        print("Initializing sampling for the "
-              + "{} cell (ID: {})...".format(num2ord(cell_order), cell_id))
+        print(f"Initializing sampling for the {num2ord(cell_order)} cell "
+              + f"(ID: {cell_id})...")
 
         # update prior distribution
         if prior_chains:
             print("Updating prior distribution from the "
-                  + "{} cell (ID: ".format(num2ord(cell_order - 1))
-                  + "{})...".format(prior_id))
+                  + f"{num2ord(cell_order - 1)} cell (ID: {prior_id})...")
             prior_mean, prior_std = get_prior_from_sample_files(prior_dir,
                                                                 prior_chains)
 
             if prior_std_scale != 1.0:
                 print("Scaling standard deviation of prior distribution "
-                      + "by {}...".format(prior_std_scale))
+                      + f"by {prior_std_scale}...")
                 prior_std *= prior_std_scale
+
+            # restrict standard deviation of prior
+            prior_std = np.clip(prior_std, 1e-3, 5)
 
             prior_chains = None  # reset prior chains
         else:
             print("Prior distribution will not be updated from the "
-                  + "{} cell (ID: ".format(num2ord(cell_order - 1))
-                  + "{}) due to too many unsuccessful ".format(prior_id)
-                  + "attempts of sampling")
+                  + f"{num2ord(cell_order - 1)} cell (ID: {prior_id}) "
+                  + "due to too many unsuccessful attempts of sampling")
 
         # gather prepared data
-        y0 = np.array([0, 0, 0.7, y[cell_id, t0]])
-        y_ref = [None, None, None, y[cell_id, t0 + 1:]]
+        y0 = np.array([0, 0, 0.7, y0_ca[cell_id]])
+        y_ref = [None, None, None, y[cell_id, :]]
         calcium_data = {
             "N": 4,
             "T": T,
@@ -122,16 +115,14 @@ def main():
         # try sampling
         while not prior_chains and num_tries < max_num_tries:
             num_tries += 1
-            print("Starting {} attempt of ".format(num2ord(num_tries))
-                  + "sampling...")
+            print(f"Starting {num2ord(num_tries)} attempt of sampling...")
             sys.stdout.flush()
 
             # run Stan session
-            stan_session = StanSession(stan_model, cell_dir, data=calcium_data,
-                                       num_chains=num_chains,
-                                       num_iters=num_iters, warmup=warmup,
-                                       thin=thin,
-                                       rhat_upper_bound=rhat_upper_bound)
+            stan_session = StanSession(
+                stan_model, cell_dir, data=calcium_data,
+                num_chains=num_chains, num_iters=num_iters, warmup=warmup,
+                thin=thin, rhat_upper_bound=rhat_upper_bound)
             stan_session.run_sampling(control=control)
             stan_session.gather_fit_result()
 
@@ -142,7 +133,7 @@ def main():
                 # good R_hat value of one chain combo
                 # analyze result of current cell
                 print("Good R_hat value of log posteriors for the "
-                      + "{} cell (ID: {})".format(num2ord(cell_order), cell_id))
+                      + f"{num2ord(cell_order)} cell (ID: {cell_id})")
                 print("Running analysis on sampled result...")
 
                 analyzer = StanSessionAnalyzer(
@@ -154,8 +145,8 @@ def main():
             else:
                 # bad R_hat value of every chain combo
                 print("Bad R_hat value of log posteriors for the "
-                      + "{} cell (ID: {})".format(num2ord(cell_order), cell_id)
-                      + "on the {} attempt".format(num2ord(num_tries)))
+                      + f"{num2ord(cell_order)} cell (ID: {cell_id}) on the "
+                      + f"{num2ord(num_tries)} attempt")
 
             print()
             sys.stdout.flush()
@@ -171,43 +162,30 @@ def get_args():
     """Parse command line arguments"""
     arg_parser = argparse.ArgumentParser(
         description="Infer parameters of calclium model for multiple cells "
-                    + "using Stan"
-    )
-    arg_parser.add_argument("--stan_model", dest="stan_model", metavar="MODEL",
-                            type=str, required=True)
-    arg_parser.add_argument("--ode_variant", dest="ode_variant", type=str,
-                            default="original",
-                            choices=["original", "equiv", "const"])
-    arg_parser.add_argument("--cell_list", dest="cell_list", type=str,
+                    + "using Stan")
+    arg_parser.add_argument("--stan_model", metavar="MODEL", type=str,
                             required=True)
-    arg_parser.add_argument("--prior_cell", dest="prior_cell", type=int,
-                            default=0)
-    arg_parser.add_argument("--prior_chains", dest = "prior_chains", type=int,
-                            nargs="+", default=[2])
-    arg_parser.add_argument("--num_cells", dest="num_cells", type=int,
-                            default=100)
-    arg_parser.add_argument("--filter_type", dest="filter_type",
-                            choices=["none", "moving_average"], default="none")
-    arg_parser.add_argument("--moving_average_window",
-                            dest="moving_average_window", type=int, default=20)
-    arg_parser.add_argument("--t0", dest="t0", metavar="T", type=int,
-                            default=200)
-    arg_parser.add_argument("--num_chains", dest="num_chains", type=int,
-                            default=4)
-    arg_parser.add_argument("--num_iters", dest="num_iters", type=int,
-                            default=2000)
-    arg_parser.add_argument("--warmup", dest="warmup", type=int, default=1000)
-    arg_parser.add_argument("--thin", dest="thin", type=int, default=1)
-    arg_parser.add_argument("--adapt_delta", dest="adapt_delta", type=float,
-                            default=0.8)
-    arg_parser.add_argument("--max_treedepth", dest="max_treedepth", type=int,
-                            default=10)
-    arg_parser.add_argument("--rhat_upper_bound", dest="rhat_upper_bound",
-                            type=float, default=1.1)
-    arg_parser.add_argument("--result_dir", dest="result_dir", metavar="DIR",
-                            type=str, default=".")
-    arg_parser.add_argument("--prior_std_scale", dest="prior_std_scale",
-                            type=float, default=1.0)
+    arg_parser.add_argument("--ode_variant", type=str, default="vanilla",
+                            choices=["vanilla", "equiv"])
+    arg_parser.add_argument("--cell_list", type=str, required=True)
+    arg_parser.add_argument("--num_cells", type=int, default=100)
+    arg_parser.add_argument("--filter_type", default=None,
+                            choices=["moving_average"])
+    arg_parser.add_argument("--moving_average_window", type=int, default=20)
+    arg_parser.add_argument("--t0", type=int, default=200)
+    arg_parser.add_argument("--downsample_offset", type=int, default=300)
+    arg_parser.add_argument("--prior_cell", type=int, default=0)
+    arg_parser.add_argument("--prior_chains", type=int, nargs="+",
+                            default=[2])
+    arg_parser.add_argument("--prior_std_scale",  type=float, default=1.0)
+    arg_parser.add_argument("--num_chains", type=int, default=4)
+    arg_parser.add_argument("--num_iters", type=int, default=2000)
+    arg_parser.add_argument("--warmup", type=int, default=1000)
+    arg_parser.add_argument("--thin", type=int, default=1)
+    arg_parser.add_argument("--adapt_delta", type=float, default=0.8)
+    arg_parser.add_argument("--max_treedepth", type=int, default=10)
+    arg_parser.add_argument("--rhat_upper_bound", type=float, default=1.1)
+    arg_parser.add_argument("--result_dir", type=str, required=True)
 
     return arg_parser.parse_args()
 
